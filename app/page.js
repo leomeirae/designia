aa35dd92b25999e30f589f6ee56416a97cf1948a
seu-usuario <seu-email@exemplo.com>
2024-12-19 14:13:48 -0300

    slides

--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 import Header from "./dashboard/_components/Header";
+import ReactBeforeSliderComponent from 'react-before-after-slider-component';
+import 'react-before-after-slider-component/dist/build.css';
 
 export default function Home() {
   return (
@@ -33,7 +37,7 @@ export default function Home() {
     <div className="mt-8 gap-3 flex justify-center">
       <a className="inline-flex justify-center items-center 
       gap-x-4 text-center bg-gradient-to-tl from-blue-600
-       to-violet-600 hover:from-violet-600 hover:to-blue-600 border border-transparent text-white text-sm font-medium rounded-md focus:outline-none focus:ring-1 focus:ring-gray-600 py-3 px-4 dark:focus:ring-offset-gray-800" 
+       to-violet-600 hover:from-violet-600 hover:to-blue-600 border border-transparent text-white text-lg font-medium rounded-md focus:outline-none focus:ring-1 focus:ring-gray-600 py-3 px-4 dark:focus:ring-offset-gray-800" 
        href="/dashboard">
         Comece agora gratuitamente!
         <svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
@@ -41,6 +45,31 @@ export default function Home() {
      
     </div>
    
+    <div className="mt-14 flex justify-center gap-8">
+      <div className="flex-1 max-w-lg">
+        <ReactBeforeSliderComponent
+          firstImage={{
+            imageUrl: '/after.png'
+          }}
+          secondImage={{
+            imageUrl: '/before.png'
+          }}
+          className="custom-slider"
+        />
+      </div>
+
+      <div className="flex-1 max-w-lg">
+        <ReactBeforeSliderComponent
+          firstImage={{
+            imageUrl: '/after1.jpg'
+          }}
+          secondImage={{
+            imageUrl: '/before1.png'
+          }}
+          className="custom-slider"
+        />
+      </div>
+    </div>
 
    
   </div>
